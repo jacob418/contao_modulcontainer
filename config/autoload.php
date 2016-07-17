d08360680e724cb732af8ea673ce758678a8582c
Jacob Fricke <jacob@http418.name>
2016-07-17 15:33:13 +0200

    Add 'autoload.php'
    Auto generated 'autoload.php' by Contao

---
+++ config/autoload.php
@@ -0,0 +1,21 @@
+<?php
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	// Modules
+	'Contao\ModuleContainerModule' => 'system/modules/modulcontainer/modules/ModuleContainerModule.php',
+));
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'mod_containermodule' => 'system/modules/modulcontainer/templates',
+	'container_default'   => 'system/modules/modulcontainer/templates',
+));
